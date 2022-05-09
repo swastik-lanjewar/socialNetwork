405b3e0ff9eb38ff7182f507f942260208aa0940
Aniket Singh <singhdharmvir81@gmail.com>
2022-05-09 20:14:41 +0530

    Added: Tailwind CSS

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Login page</h1>
+<section class="mx-auto w-1/2">
+  <form @submit.prevent="" class="bg-gray-100 p-6">
+  <h1 class="bold">Login page</h1>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" v-model="password" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary" @click="login">Login</button>
+  </form>
+</section>
 </template>
 
 <script>
