459ae80738c47f2e14c130013049d23179086153
Aniket Singh <singhdharmvir81@gmail.com>
2022-05-10 16:21:03 +0530

    ADDED: created login and create account page in the frontend

--- client/src/views/CreateAccountView.vue
+++ client/src/views/CreateAccountView.vue
@@ -1,5 +1,46 @@
 <template>
-  <h2>Create Account</h2>
+
+<section class="w-full h-screen flex justify-center content-center bg-gray-50">
+  <form @submit.prevent="" class="rounded bg-white  w-fit h-fit p-6 shadow-md">
+    <h1 class=" text-2xl mb-4">Social Network | Create Account</h1>
+    <div class="mb-4">
+      <label for="name" class="block text-gray-700 text-sm font-bold mb-2">
+        Name
+      </label>
+      <input type="text" id="name" class="w-full border border-gray-300 rounded px-2 py-1 ">
+    </div>
+    <div class="mb-4">
+      <label for="username" class="block text-gray-700 text-sm font-bold mb-2">
+        Username
+      </label>
+      <input type="text" id="username" class="w-full border border-gray-300 rounded px-2 py-1 ">
+    </div>
+    <div class="mb-4">
+      <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+        Email
+      </label>
+      <input type="email" id="email" class="w-full border border-gray-300 rounded px-2 py-1 ">
+    </div>
+    <div class="mb-4">
+      <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
+        Password
+      </label>
+      <input type="password" id="password" class="w-full border border-gray-300 rounded  px-2 py-1">
+    </div>
+    <div class="mb-4">
+      <label for="cpassword" class="block text-gray-700 text-sm font-bold mb-2">
+        Confirm Password
+      </label>
+      <input type="password" id="cpassword" class="w-full border border-gray-300 rounded  px-2 py-1">
+    </div>
+    <div class="mt-4 mb-2 flex justify-center">
+      <button type="submit" class="bg-blue-400 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded">
+       Create Account
+    </button>
+    </div>
+    <p>Already Created?<a class="text-blue-600"> Login</a></p>
+  </form>
+</section>
 </template>
 
 <script>
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,18 +1,29 @@
 <template>
-<section class="mx-auto w-1/2">
-  <form @submit.prevent="" class="bg-gray-100 p-6">
-  <h1 class="bold">Login page</h1>
-    <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email">
+
+<section class="w-full h-screen flex justify-center content-center bg-gray-50">
+  <form @submit.prevent="" class="rounded bg-white  w-fit h-fit p-6 shadow-md">
+    <h1 class=" text-2xl mb-4">Social Network | Login</h1>
+    <div class="mb-4">
+      <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+        Email
+      </label>
+      <input type="email" id="email" class="w-full border border-gray-300 rounded px-2 py-1 ">
     </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" v-model="password" placeholder="Password">
+    <div class="mb-4">
+      <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
+        Password
+      </label>
+      <input type="password" id="password" class="w-full border border-gray-300 rounded  px-2 py-1">
     </div>
-    <button type="submit" class="btn btn-primary" @click="login">Login</button>
+    <div class="mt-4 mb-2 flex justify-center">
+      <button type="submit" class="bg-blue-400 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded">
+        Login
+    </button>
+    </div>
+    <p>Don't have accout? <a class="text-blue-600">Create Account</a></p>
   </form>
 </section>
+
 </template>
 
 <script>
