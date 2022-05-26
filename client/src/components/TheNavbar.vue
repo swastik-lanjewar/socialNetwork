17762fb8cd1d72a065219f09a8e6e40a6c2c3ff6
Aniket Singh <singhdharmvir81@gmail.com>
2022-05-26 17:55:12 +0530

    Added changes

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -10,50 +10,15 @@
             <i :class="'fab fa-solid ' + item.icon"></i>
           </router-link>
         </li>
+        <li>
+          <button>
+            <i class="fas fa-solid fa-bell"></i>
+          </button>
+        </li>
       </ul>
     </div>
     <div class="mr-7 flex">
-      <label
-        for="search"
-        class="
-          relative
-          text-slate-400
-          focus-within:text-gray-600
-          hidden
-          md:block
-        "
-      >
-        <span
-          class="
-            pointer-events-none
-            w-8
-            h-8
-            absolute
-            top-1/2
-            transform
-            -translate-y-1/2
-            left-0
-            grid
-            place-items-center
-          "
-          ><i class="fab fa-solid fa-magnifying-glass"></i
-        ></span>
-        <input
-          type="search"
-          id="search"
-          class="
-            bg-slate-200
-            form-input
-            w-full
-            px-4
-            pl-8
-            rounded-md
-            py-1
-            focus:outline-none
-          "
-          placeholder="Search People"
-        />
-      </label>
+     <TheSearchBar />
       <button class="ml-5" @click="logout">
         <i class="fas fa-solid fa-sign-out text-blue-600"></i>
       </button>
@@ -62,42 +27,43 @@
 </template>
 
 <script>
+import TheSearchBar from './utils/TheSearchBar.vue';
 export default {
-  name: "TheNavbar",
-  data() {
-    return {
-      navItems: [
-        {
-          name: "Home",
-          icon: "fa-house",
-          link: "/",
+    name: "TheNavbar",
+    data() {
+        return {
+            searchText: "",
+            navItems: [
+                {
+                    name: "Home",
+                    icon: "fa-house",
+                    link: "/",
+                },
+                {
+                    name: "friends",
+                    icon: "fa-user-group",
+                    link: "/people",
+                },
+                {
+                    name: "message",
+                    icon: "fa-comment",
+                    link: "/message",
+                },
+            ],
+        };
+    },
+    methods: {
+        logout() {
+            // delelte the token from local storage
+            localStorage.removeItem("token");
+            // redirect to login page
+            this.$router.push("/login");
         },
-        {
-          name: "friends",
-          icon: "fa-user-group",
-          link: "/people",
-        },
-        {
-          name: "notification",
-          icon: "fa-bell",
-          link: "",
-        },
-        {
-          name: "message",
-          icon: "fa-comment",
-          link: "/message",
-        },
-      ],
-    };
-  },
-  methods:{
-    logout(){
-      // delelte the token from local storage
-      localStorage.removeItem('token');
-      // redirect to login page
-      this.$router.push('/login');
-    }
-  }
+        searchUser() {
+            console.log(this.searchText);
+        }
+    },
+    components: { TheSearchBar }
 };
 </script>
 
